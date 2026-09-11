8e702c722f7b491e941fa073606c0211bbade393
kwecko <kwecko@gmail.com>
2026-09-11 15:33:21 -0300

    Implementa validação dos grupos

--- usuarios-grupos/verify-step1.sh
+++ usuarios-grupos/verify-step1.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-echo "TESTE: A VALIDACAO FOI EXECUTADA"
-exit 1
+ERROS=0
+
+for grupo in administracao suporte desenvolvimento
+do
+    if getent group "$grupo" > /dev/null 2>&1; then
+        :
+    else
+        ERROS=$((ERROS + 1))
+    fi
+done
+
+if [ "$ERROS" -eq 0 ]; then
+    exit 0
+else
+    exit 1
+fi
